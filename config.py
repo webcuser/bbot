ea320d9c5d7735709e38795ea3932fc0b8fec71f
Serghei <serghei.cucereavii@r2u.it>
2024-09-19 15:36:55 +0200

    [fix] name file

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_KEY = ''
-API_SECRET = ''
+API_KEY = '9ItaNhVvo3ozWveuwj'
+API_SECRET = 'Pz4rj2RBOJkPq2wR96V7lN6ZXmvcDRe4pnQA'
 ACCOUNT = 'UNIFIED' # only unyfied
 SYMBOL = "DOGSUSDT" #add pire like DOGSUSDT or CATSUSDT for spot trade
 LISTING_DATE = "19/09/2024 14:39"  # everytime like "19/09/2024 14:39" format
